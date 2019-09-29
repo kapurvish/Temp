5293943b203e413bc734f88fd10a23a1c815a30d
KapurV <38396421+kapurvish@users.noreply.github.com>
2019-09-29 22:50:07 +0530

    Update cnn_assign1.py

--- cnn_assign1.py
+++ cnn_assign1.py
@@ -68,3 +68,63 @@ model.fit(train_images, train_labels,epochs=5)
 
 print(model.summary())
 
+model =tf.keras.models.Sequential()
+
+model.add(tf.keras.layers.Conv2D(64,(3,3),activation='relu',input_shape=(32,32,3)))
+model.add(tf.keras.layers.MaxPooling2D((2,2)))
+model.add(tf.keras.layers.Conv2D(32,(3,3),activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D((2,2)))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(64,activation ='relu'))
+model.add(tf.keras.layers.Dense(10,activation ='softmax'))
+
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+model.fit(train_images, train_labels,epochs=10)
+
+#Epoch 1/10
+#50000/50000 [==============================] - 46s 919us/sample - loss: 1.5025 - accuracy: 0.4580: 7s - loss: 1.5413 - accuracy: 0.4418
+#Epoch 2/10
+#50000/50000 [==============================] - 45s 906us/sample - loss: 1.1601 - accuracy: 0.5933
+#Epoch 3/10
+#50000/50000 [==============================] - 44s 881us/sample - loss: 1.0337 - accuracy: 0.6380
+#Epoch 4/10
+#50000/50000 [==============================] - 44s 880us/sample - loss: 0.9598 - accuracy: 0.6669
+#Epoch 5/10
+#50000/50000 [==============================] - 44s 879us/sample - loss: 0.9009 - accuracy: 0.6861
+#Epoch 6/10
+#50000/50000 [==============================] - 44s 882us/sample - loss: 0.8562 - accuracy: 0.7020
+#Epoch 7/10
+#50000/50000 [==============================] - 45s 904us/sample - loss: 0.8172 - accuracy: 0.7160
+#Epoch 8/10
+#50000/50000 [==============================] - 44s 879us/sample - loss: 0.7854 - accuracy: 0.7269
+#Epoch 9/10
+#50000/50000 [==============================] - 44s 888us/sample - loss: 0.7546 - accuracy: 0.7368
+#Epoch 10/10
+#50000/50000 [==============================] - 44s 878us/sample - loss: 0.7223 - accuracy: 0.7480
+#Model: "sequential"
+#_________________________________________________________________
+#Layer (type)                 Output Shape              Param #   
+#=================================================================
+#conv2d (Conv2D)              (None, 30, 30, 64)        1792      
+#_________________________________________________________________
+#max_pooling2d (MaxPooling2D) (None, 15, 15, 64)        0         
+#_________________________________________________________________
+#conv2d_1 (Conv2D)            (None, 13, 13, 32)        18464     
+#_________________________________________________________________
+#max_pooling2d_1 (MaxPooling2 (None, 6, 6, 32)          0         
+#_________________________________________________________________
+#flatten (Flatten)            (None, 1152)              0         
+#_________________________________________________________________
+#dense (Dense)                (None, 64)                73792     
+#_________________________________________________________________
+#dense_1 (Dense)              (None, 10)                650       
+#=================================================================
+#Total params: 94,698
+#Trainable params: 94,698
+#Non-trainable params: 0
+#_________________________________________________________________
+#None
+
+#Answer for Ques 2 should be  70-75 , C option.
+#Answer for Ques 3 should be  Reduced by 94,698 , b option.
+
